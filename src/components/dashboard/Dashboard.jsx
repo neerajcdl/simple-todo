ac3956d322dc921d672f8b436da3c1b176f994db
neerajcdl <neerajcdlsiet@gmail.com>
2024-12-03 21:02:44 +0530

    [IMP] added search functionality

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../dashboard/Dashboard.css";
 
 const Dashboard = (props) => {
   const { todoData, setTodoData, setIsEditing, setCurrentIndex } = props;
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleDelete = (id) => {
     const deleteData = todoData.filter((_, index) => id !== index);
@@ -25,30 +26,54 @@ const Dashboard = (props) => {
     setTodoData(doneData);
   };
 
+  // search
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filterData = () => {
+    const query = searchQuery.toLowerCase().trim();
+    return todoData.filter(
+      (item) =>
+        item.name.toLowerCase().includes(query) |
+        item.age.toLowerCase().includes(query) |
+        item.gender.toLowerCase().includes(query) |
+        item.venderId.toLowerCase().includes(query)
+    );
+  };
+
   return (
-    <div>
-      {todoData.map((item, index) => {
-        return (
-          <div key={index} className="dashboard-data-container">
-            <div
-              className="dashboard-data"
-              style={{
-                textDecoration: item.completed ? "line-through" : "",
-              }}
-            >
-              <p>{item.name}</p>
-              <p>{item.age}</p>
-              <p>{item.gender}</p>
-              <p>{item.venderId}</p>
+    <div className="dashboard-container">
+      <div className="search-input-field">
+        <input placeholder="search" onChange={handleSearch} />
+      </div>
+      {filterData().length === 0 ? (
+        <div className="no-data-part">No Data</div>
+      ) : (
+        filterData().map((item, index) => {
+          return (
+            <div key={index} className="dashboard-data-container">
+              <div
+                className="dashboard-data"
+                style={{
+                  textDecoration: item.completed ? "line-through" : "",
+                }}
+              >
+                <p>{item.name}</p>
+                <p>{item.age}</p>
+                <p>{item.gender}</p>
+                <p>{item.venderId}</p>
+              </div>
+              <div className="dashboard-btn-container">
+                <button onClick={() => handleDelete(index)}>Delete</button>
+                <button onClick={() => handleEdit(index)}>Edit</button>
+                <button onClick={() => handleDone(index)}>Done</button>
+              </div>
             </div>
-            <div className="dashboard-btn-container">
-              <button onClick={() => handleDelete(index)}>Delete</button>
-              <button onClick={() => handleEdit(index)}>Edit</button>
-              <button onClick={() => handleDone(index)}>Done</button>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 };
